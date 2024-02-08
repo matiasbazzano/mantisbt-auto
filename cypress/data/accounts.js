0e64ee6a51f861b47b5d0139c6395c06e9be4868
Matias Bazzano <matiasbazzano19@gmail.com>
2024-02-08 12:07:45 -0300

    Updated changes
    - accounts.js: Valid and invalid data for login
    - baseUrl.js: Base url page
    - deepUrl.js: Deep urls page

---
+++ cypress/data/accounts.js
@@ -0,0 +1,8 @@
+const AccountData = {
+    validUser: 'mbazzano',
+    validPassword: 'testing123',
+    invalidUser: 'invalidUser1906',
+    invalidPassword: 'invalidPass123',
+  };
+  
+  export default AccountData;
