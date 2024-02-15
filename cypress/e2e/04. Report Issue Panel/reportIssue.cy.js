7de206574c3594ed69afeabaa791a21aab980b62
Matias Bazzano <matiasbazzano19@gmail.com>
2024-02-15 00:24:44 -0300

    Updated changes

--- cypress/e2e/04. Report Issue Panel/reportIssue.cy.js
+++ cypress/e2e/04. Report Issue Panel/reportIssue.cy.js
@@ -13,7 +13,7 @@ describe("Mantis BT - Report Issue Page Tests", () => {
       LoginPage.login(AccountData.validUser, AccountData.validPassword);
     });
 
-      it("@Smoke - Report New Issue", () => {
+      it("@Smoke - Report New Issue - mantisbt Project", () => {
         Header.reportIssueButton.click();
         ChooseProjectPage.selectProjectDropdown.select('1');
         ChooseProjectPage.selectProjectButton.click();
@@ -32,4 +32,59 @@ describe("Mantis BT - Report Issue Page Tests", () => {
         IssueDetailsPage.submitIssueButton.click();
         cy.url().should("include", DeepUrl.viewIssueDetals);
       });
+
+      it("@Smoke - Report New Issue - Mylyn Connector Project", () => {
+        Header.reportIssueButton.click();
+        ChooseProjectPage.selectProjectDropdown.select('20');
+        ChooseProjectPage.selectProjectButton.click();
+        IssueDetailsPage.categoryDropdown.select('75');
+        IssueDetailsPage.reproducibilityDropdown.select('10');
+        IssueDetailsPage.severityDropdown.select('10');
+        IssueDetailsPage.priorityDropdown.select('10');
+        IssueDetailsPage.profilePlatformInput.type(NewIssue.platform);
+        IssueDetailsPage.osPlatformInput.type(NewIssue.os);
+        IssueDetailsPage.osVersionPlatformInput.type(NewIssue.osVersion);
+        IssueDetailsPage.productVersionDropdown.select('3.0.1');
+        IssueDetailsPage.summaryInput.type(NewIssue.summary);
+        IssueDetailsPage.descriptionInput.type(NewIssue.description);
+        IssueDetailsPage.stepsToReproduce.type(NewIssue.steps);
+        IssueDetailsPage.additionalInfoInput.type(NewIssue.additionalInfo);
+        IssueDetailsPage.submitIssueButton.click();
+        cy.url().should("include", DeepUrl.viewIssueDetals);
+      });
+
+      it("@Regression - Report New Issue - Leave the required dropdowns empty (category)", () => {
+        Header.reportIssueButton.click();
+        ChooseProjectPage.selectProjectDropdown.select('20');
+        ChooseProjectPage.selectProjectButton.click();
+        cy.get('[id="category_id"]').then(($select) => {
+          expect($select[0].validationMessage).to.eq('Please select an item in the list.')
+        })
+        cy.url().should("eq", DeepUrl.bugReport);
+      });
+
+      it("@Regression - Report New Issue - Leave the required fields empty (summary) ", () => {
+        Header.reportIssueButton.click();
+        ChooseProjectPage.selectProjectDropdown.select('20');
+        ChooseProjectPage.selectProjectButton.click();
+        IssueDetailsPage.categoryDropdown.select('75');
+        IssueDetailsPage.reproducibilityDropdown.select('10');
+        cy.get('[id="summary"]').then(($input) => {
+          expect($input[0].validationMessage).to.eq('Please fill out this field.')
+        })
+        cy.url().should("eq", DeepUrl.bugReport);
+      });
+
+      it("@Regression - Report New Issue - Leave the required fields empty (description)", () => {
+        Header.reportIssueButton.click();
+        ChooseProjectPage.selectProjectDropdown.select('20');
+        ChooseProjectPage.selectProjectButton.click();
+        IssueDetailsPage.categoryDropdown.select('75');
+        cy.get('[id="description"]').then(($input) => {
+          expect($input[0].validationMessage).to.eq('Please fill out this field.')
+        })
+        cy.url().should("eq", DeepUrl.bugReport);
+      });
+
+      //do a test to check the status of an issue created, private/public in viewIssueDetails (involves pageobject and saving the private and public checkbox)
   });
--- cypress/support/pageObjects/chooseProject.page.js
+++ cypress/support/pageObjects/chooseProject.page.js
@@ -7,14 +7,6 @@ class ChooseProjectPage {
       get selectProjectButton() {
         return cy.get('input[type="submit"].btn.btn-primary.btn-white.btn-round[value="Select Project"]');
       }
-
-      getProjectOption(value) {
-        return this.selectProjectDropdown.find(`option[value="${value}"]`);
-      }
-    
-      selectProject(value) {
-        this.selectProjectDropdown.select(value);
-      }
     }
     
 export default new ChooseProjectPage();
